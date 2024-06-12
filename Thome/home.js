2be88efef5ae11731fb38378559bf7b1745baa0d
Tejaswini Kale <119682227+tejaswini1304@users.noreply.github.com>
2024-06-12 13:25:24 +0530

    Add files via upload

---
+++ Thome/home.js
@@ -0,0 +1,8 @@
+function redirectToGradesheet() {
+    window.location.href = 'Marks/marks.php';
+  }
+  
+  function redirectToResults() {
+    window.location.href = 'Topper/results.php';
+  }
+  
---
+++ Thome/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Teacher Home</title>
+  <link rel="stylesheet" href="home.css">
+</head>
+<body>
+<div id="container">
+  <div class="content">
+    <div class="left-half">
+      <img src="home.jpg" alt="Teacher Image">
+    </div>
+    <div class="right-half">
+      <div class="buttons">
+        <button id="gradesheetButton" onclick="redirectToGradesheet()">Make Gradesheet</button>
+        <button id="resultsButton" onclick="redirectToResults()">See Results</button>
+      </div>
+    </div>
+  </div>
+</div>
+  <script src="home.js"></script>
+</body>
+</html>
