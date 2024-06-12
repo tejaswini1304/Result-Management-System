3dc4fa399497435e8ebebfab2f88fd8474c7c98c
Tejaswini Kale <119682227+tejaswini1304@users.noreply.github.com>
2024-06-12 13:22:15 +0530

    Add files via upload

---
+++ Notice/notice.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Notice</title>
+  <link rel="stylesheet" href="notice.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+  <nav class="navbar">
+    <ul>
+      <li><a href="home.html" id="homeLink">Home</a></li>
+      <li><a href="result.html" id="resultLink">Result</a></li>
+      <li><a href="noticeboard.html" id="noticeLink" class="active">Noticeboard</a></li>
+      <li><a href="profile.html" id="profileLink">Profile</a></li>
+    </ul>
+  </nav>
+  <div class="calendar-container">
+    <p class="events-title"><strong><i class="fas fa-calendar-days"></i> ALL YOUR EVENTS ARE HERE!!</strong></p>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=UTC&bgcolor=%23ff8c6b&src=bWFuc2ljaGl0YXJpQGdtYWlsLmNvbQ&src=ZTg1MmM4MjJmNGE2YzI0YjM5ZmM2OGY5Nzg3OGMyYmMyYjk4NTNjMmUyODU2NTE5ZTQ3MWYzZDNkNzAwYmJmYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4uaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%23D50000&color=%2333B679&color=%230B8043" style="border:solid 1px #777" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+  </div>
+</body>
+</html>
